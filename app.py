e43c7e2d0dd79df40645f1118d05586a786fbea7
sara <sara27082011@gmail.com>
2021-12-10 22:20:52 -0300

    update variável formato

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from pytube import YouTube
 
 url = input("Digite o link: ")
 _filename = input("Nome do arquivo: ")
-mp3 = "'%s'.mp3" % _filename
+formato = "'%s'.mp4" % _filename
 
 print("baixando...")
-YouTube(url).streams.first().download(filename=mp3)
+YouTube(url).streams.first().download(filename=formato)
 print("Download concluído!")
 
 
